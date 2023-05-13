db2d5443ecc0f65a19e8aae44269203ee051da37
Carlos Colin <carloscolinn@outlook.es>
2023-05-12 23:11:28 -0600

    add splashcreen & home component. add styles

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,42 @@
 <template>
-  <Home />
+  <Suspense>
+    <template #default>
+      <Home />
+    </template>
+    <template #fallback>
+      <SplashScreen />
+    </template>
+  </Suspense>
 </template>
 
 <script>
-import Home from "@/components/Home";
+import SplashScreen from '@/components/SplashScreen'
+import { defineAsyncComponent } from 'vue'
 
 export default {
   components: {
-    Home,
+    SplashScreen,
+    Home: defineAsyncComponent(() => (
+      new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(require('@/components/Home'))
+        }, 2500)
+      })
+    ))
   },
-};
+}
 </script>
 
-<style></style>
+<style>
+html,
+body,
+.app {
+  min-height: 100vh;
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+* {
+  --brand-green: #04b500;
+  --brand-blue: #0689b0;
+}
+</style>
