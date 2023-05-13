2d4144a47646dff154548adb64067b89ab59dfd4
Carlos Colin <carloscolinn@outlook.es>
2023-05-12 23:11:47 -0600

    add header and resume

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,16 @@
 <template>
-  <h1>Hola mundo</h1>
+  <Layout>
+    <template #header>
+      <Header />
+    </template>
+    <template #resume>
+      <Resume />
+    </template>
+  </Layout>
 </template>
+
+<script setup>
+import Layout from '@/components/Layout'
+import Header from '@/components/Header'
+import Resume from '@/components/Resume'
+</script>
