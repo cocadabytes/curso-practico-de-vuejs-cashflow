98efcbe125bd228010bb9847580cc21860554183
Carlos Colin <carloscolinn@outlook.es>
2023-05-15 22:57:25 -0600

    create movements single element

---
+++ src/components/Movements/Element/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="movement">{{movement.title}}</div>
+</template>
+
+<script setup>
+import { toRefs, } from 'vue'
+
+
+const props = defineProps({
+  movement: {
+    type: Object,
+    required: true,
+  },
+})
+
+const { movement, } = toRefs(props)
+</script>
